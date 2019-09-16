e8bf8cbd67ba149815a86e525aa79f69bfa35716
Ethan Humphrey <ethantheinnovator@gmail.com>
2019-09-16 08:37:22 -0400

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Ethan Humphrey on 9/16/19.
+//  Copyright © 2019 Ethan Humphrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
